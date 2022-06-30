a10e73d15990d22032a4938b9737606118675430
KimberlingOrtega <1996.gorgette21ortega@gmail.com>
2022-06-30 17:25:39 +0000

    Corregido los estilos

--- src/js/component/Cards.jsx
+++ src/js/component/Cards.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Cards= () =>{
     return (
 
-        <div className="card col-md-3 col-sm-12" >
-  <img src="https://picsum.photos/seed/picsum/200/250" className="card-img-top w-400px h-200px" alt="..."/>
+        <div className="card h-100" >
+  <img src="https://picsum.photos/seed/picsum/200/250" className="card-img-top" alt="..."/>
   <div className="card-body">
     <h5 className="card-title">Card title</h5>
     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
